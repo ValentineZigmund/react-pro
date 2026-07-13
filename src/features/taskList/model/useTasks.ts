97442512bb84ee9ae77a465a2b4ef8cc6377382d
Валентин Зигмунд <valentinzigmund@gmail.com>
2026-07-13 14:32:44 +0300

    lesson-2

--- src/features/taskList/model/useTasks.ts
+++ src/features/taskList/model/useTasks.ts
@@ -1,5 +1,5 @@
 import type { Task } from 'entities/task/model/types';
-import { useEffect, useState } from 'react';
+import { useMemo, useCallback, useState } from 'react';
 
 export const FILTER = {
   all: 'all',
@@ -9,31 +9,26 @@ export const FILTER = {
 
 export type Filter = keyof typeof FILTER;
 
-const TASKS: Task[] = [
-  { id: '0', title: 'one', completed: false },
-  { id: '1', title: 'two', completed: false },
-  { id: '2', title: 'three', completed: true }
-]
+export const TASKS: Task[] = Array.from({ length: 1000 }, (_, index) =>
+  ({ id: String(index), title: String(index), completed: Boolean(index % 2) }))
 
 const statusToBool = (status: Filter) =>
   status === FILTER.completed
 
-
 export const useTasks = (initial: Task[] = TASKS) => {
 
+  console.log('render')
+
   const [initialTasks, setInitialTasks] = useState(initial)
-  const [tasks, setTasks] = useState<Task[]>([])
   const [filter, setFilter] = useState<Filter>(FILTER.all)
 
-  useEffect(() => {
-    // Если фильтр установлен на 'all' - отображаем все таски
-    if (filter === FILTER.all) return setTasks(initialTasks)
-    // В противном случае - оставляем только соотвествующие фильтру
-    setTasks(initialTasks.filter((task) => task.completed === statusToBool(filter)))
-  }, [initialTasks, filter])
+  const removeTask = useCallback((id: string) =>
+    setInitialTasks(initialTasks.filter(task => task.id !== id)), [initialTasks])
 
-  const removeTask = (id: string) =>
-    setInitialTasks(initialTasks.filter(task => task.id !== id))
+  const tasks = useMemo(() => {
+    if (filter === FILTER.all) return initialTasks
+    return initialTasks.filter((task) => task.completed === statusToBool(filter))
+  }, [initialTasks, filter])
 
   return { tasks, setFilter, removeTask }
 }
